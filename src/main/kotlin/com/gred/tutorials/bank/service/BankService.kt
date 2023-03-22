c90d3da722e0110ded272edb78061b45e89eb02a
brokoli <brokoli@marketboro.com>
2023-03-22 15:17:41 +0900

    Service 및 ServiceTest 추가

---
+++ src/main/kotlin/com/gred/tutorials/bank/service/BankService.kt
@@ -0,0 +1,10 @@
+package com.gred.tutorials.bank.service
+
+import com.gred.tutorials.bank.datasource.BankDataSource
+import com.gred.tutorials.bank.model.Bank
+import org.springframework.stereotype.Service
+
+@Service
+class BankService(private val dataSource: BankDataSource) {
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+}
